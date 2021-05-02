640a640527de489d89e030c025f913c0946eb4e4
dilbert <chrtimm@web.de>
2021-05-02 09:47:33 +0200

    Task 2: implement rule of five

--- src/chatbot.h
+++ src/chatbot.h
@@ -29,7 +29,10 @@ public:
 
     //// STUDENT CODE
     ////
-
+    ChatBot(const ChatBot& src);              // copy constructor
+    ChatBot& operator = (const ChatBot& src); // assignment operator
+    ChatBot(ChatBot &&source);                // 4 : move constructor
+    ChatBot &operator=(ChatBot &&source);     // 5 : move assignment operator
     ////
     //// EOF STUDENT CODE
 
